2fa60136610e0d8154c93f3cd689c21a82082e0d
Gourav <gourav@gmail.com>
2021-03-19 11:16:54 +0530

    first data save to atlas

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const User = mongoose.model('User')
+
+const router = express.Router()
+
+router.post('/signup', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = new User({email, password})
+    await user.save();
+
+    res.send('You made a post request')
+})
+
+module.exports = router
