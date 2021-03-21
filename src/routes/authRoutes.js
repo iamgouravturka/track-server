506fc4d74783d026bfbd6867568d27e9d66c560f
Gourav <gourav@gmail.com>
2021-03-21 09:56:32 +0530

    signin api working now

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -13,10 +13,32 @@ router.post('/signup', async (req, res) => {
         await user.save();
 
         const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY')
-        res.send({token})
+        res.send({ token })
 
     }catch(err) {
         res.status(422).send(err.message)
+    }
+})
+
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body
+
+    if(!email || !password) {
+        return res.status(422).send({ error: 'Must provide email and password' })
+    }
+
+    const user = await User.findOne({ email })
+
+    if(!user)
+        return res.status(422).send({ error: 'Invalid password or email' })
+
+    try{
+        await user.comparePassword(password)
+        const token = jwt.sign({ userId: user._id}, 'MY_SECRET_KEY')
+
+        res.send({ token })
+    }catch(err){
+        return res.status(422).send({ error: 'Invalid password or email' })
     }
 })
 
